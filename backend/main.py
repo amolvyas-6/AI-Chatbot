96e187142861eec300f277fce637f4c310c1d962
Amol Vyas <amol.vyas0630@gmail.com>
2025-07-17 16:47:46 +0530

    added backend

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,10 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from flask_cors import CORS
 from graph import agent, State
 from langchain_core.messages import BaseMessage, HumanMessage, AIMessage
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/")
 def hello_world():
@@ -10,7 +12,10 @@ def hello_world():
 
 @app.route("/chatbot")
 def get_response():
-    initState = State(messages=[HumanMessage(content="Hi! My name is Amol. What can you do?")])
-    response = agent.invoke(initState)
-    print(response)
-    return f"<p> {response["messages"][-1]} </p>"
+    prompt = request.args.get("prompt")
+    initState = State(messages=[])
+    initState["messages"].append(HumanMessage(content=prompt))
+    initState = agent.invoke(initState)
+    responseContent = initState["messages"][-1].content
+    responseContent = responseContent.split("</think>")[-1]
+    return jsonify(responseContent)
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,49 @@
-export default function App(){
-    return (
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import axios from "axios";
+
+export default function App() {
+  const [messages, setMessages] = useState([]);
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm();
+
+  const submitFunction = async (formData) => {
+    setMessages((prev) => [...prev, ["Human", formData.prompt]]);
+    reset();
+    try {
+      const aiMessage = await axios.get("http://127.0.0.1:5000/chatbot", {
+        params: { prompt: formData.prompt },
+      });
+      setMessages((prev) => [...prev, ["AI", aiMessage.data]]);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  return (
     <>
-        <h1>Hello World!</h1>
-        <input type="audio" />
+      <h1>Job Recommendation System</h1>
+
+      <ul>
+        {messages.map(([role, text], idx) => (
+          <li key={idx}>
+            <strong>{role}: </strong> {text}
+          </li>
+        ))}
+      </ul>
+
+      <form onSubmit={handleSubmit(submitFunction)}>
+        <input
+          type="text"
+          placeholder="Enter prompt"
+          {...register("prompt", { required: true })}
+        />
+        <button type="submit">Submit Prompt</button>
+      </form>
     </>
-    );
+  );
 }
